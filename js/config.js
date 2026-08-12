b8d1ebec6d23994f58c9a11850ac0fe3bff877c1
superj107 <113813090+superj107@users.noreply.github.com>
2026-08-12 17:46:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyHB3sji2j7CI1FWcxsWlUsyPzprtJ4CuyAK5NPdfm-QxeAh8tulYjOhWoo2S1pt6EG/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbw_4kivWogp9zh4Qp4dhyVXAN5vQntrwjT4A4TUQhBu-kfbHfg-KQC2eTaQCVYaQcWb/exec'
 };
